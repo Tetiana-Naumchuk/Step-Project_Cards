0e5e3ff4ec480206f36e8e5804f0f6087589f720
Дарья Курносова <iamkurnos@gmail.com>
2023-03-11 12:48:08 +0200

    create form template

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+import Modal from "./modal.js"
 
 const btnEnter = document.querySelector('.enter-btn')
 
 btnEnter.addEventListener('click', () => {
-    
+    const modalEnter = new Modal().enter()
+    document.body.prepend(modalEnter)
 })
--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,21 @@
-class Modal{
-    
+export default class Modal{
+    constructor() {
+        const formEnterTemplate = document.querySelector('#form-template').content
+        this.form = formEnterTemplate.querySelector('.form').cloneNode(true)
+        this.form.classList.add('active-form')
+        this.form.addEventListener('click', (event) => {
+            if (event.target.classList.contains('form') || event.target.classList.contains('close-form')) {
+                this.form.classList.remove('active-form')
+            }
+        })
+    }
+
+    enter() {
+        
+        return this.form
+    }
+
+    card() {
+        return this.form
+    }
 }
