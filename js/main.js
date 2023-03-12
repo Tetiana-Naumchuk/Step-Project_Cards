62c498751bff76ad3ce6cc7bb9ff99e38d3f1a14
Дарья Курносова <iamkurnos@gmail.com>
2023-03-12 18:39:12 +0200

    add styles

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ import Modal from "./modal.js"
 
 export const btnEnter = document.querySelector('.enter-btn')
 
+if (localStorage.getItem('token')) {
+    btnEnter.className = 'create-visit'
+    btnEnter.textContent = 'Создать визит'
+    
+} 
+
 btnEnter.addEventListener('click', () => {
     const modalEnter = new Modal().enter()
     document.body.prepend(modalEnter)
