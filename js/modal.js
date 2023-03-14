ea29a3462d73b7d1f8b8e9daa9056f603cc18bb9
Таня Наумчук <tatiana.naumchuk123456789@gmail.com>
2023-03-14 05:45:35 +0200

    render card dentist

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,44 @@
-import Requests from "./requests.js"
-import { btnEnter } from "./main.js"
+import Requests from './requests.js';
+import { btnEnter } from './main.js';
 
-export default class Modal{
-    constructor() {
-        const formEnterTemplate = document.querySelector('#form-template').content
-        this.formWrap = formEnterTemplate.querySelector('.form').cloneNode(true)
-        this.form = this.formWrap.querySelector('form')
-        this.formWrap.classList.add('active-form')
-        this.formWrap.addEventListener('click', (event) => {
-            if (event.target.classList.contains('form') || event.target.classList.contains('close-form')) {
-                this.formWrap.classList.remove('active-form')
-            }
-        })
-    }
+export default class Modal {
+	constructor() {
+		const formEnterTemplate = document.querySelector('#form-template').content;
+		this.formWrap = formEnterTemplate.querySelector('.form').cloneNode(true);
+		this.form = this.formWrap.querySelector('form');
+		this.formWrap.classList.add('active-form');
+		this.formWrap.addEventListener('click', event => {
+			if (
+				event.target.classList.contains('form') ||
+				event.target.classList.contains('close-form')
+			) {
+				this.formWrap.classList.remove('active-form');
+			}
+		});
+	}
 
-    enter() {
-        const enterFillingTemplate = document.querySelector('#form_enter-filling').content
-        const formEnter = enterFillingTemplate.querySelector('.form_enter').cloneNode(true)
-        this.form.append(formEnter)
-        this.form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            const email = this.form.querySelector('.user-email').value
-            const password = this.form.querySelector('.user-password').value
-            const userData = {
-                email,
-                password
-            }
-            Requests.enter(userData).then(token => localStorage.setItem('token', token))
-            console.log(localStorage.getItem('token'));
-            btnEnter.className = 'create-visit'
-            btnEnter.textContent = 'Створити візит'
-            this.formWrap.classList.remove('active-form')
-        })
-        return this.formWrap 
-    }
+	enter() {
+		const enterFillingTemplate = document.querySelector('#form_enter-filling').content;
+		const formEnter = enterFillingTemplate.querySelector('.form_enter').cloneNode(true);
+		this.form.append(formEnter);
+		this.form.addEventListener('submit', event => {
+			event.preventDefault();
+			const email = this.form.querySelector('.user-email').value;
+			const password = this.form.querySelector('.user-password').value;
+			const userData = {
+				email,
+				password,
+			};
+			Requests.enter(userData).then(token => localStorage.setItem('token', token));
+			console.log(localStorage.getItem('token'));
+			btnEnter.className = 'create-visit';
+			btnEnter.textContent = 'Створити візит';
+			this.formWrap.classList.remove('active-form');
+		});
+		return this.formWrap;
+	}
 
-    card() {
-        return this.formWrap
-    }
+	card() {
+		return this.formWrap;
+	}
 }
--- js/modalCreateCard.js
+++ js/modalCreateCard.js
@@ -1,146 +1,171 @@
-import Requests from "./requests.js";
+import Requests from './requests.js';
+import { Visit } from './visit.js';
+import { VisitDentist } from './visit.js';
+
+const cardContainer = document.querySelector('.cards-container');
+
 export default class ModalCreateVisit {
-  constructor() {
-    this.dopInfoWrapper = document.createElement("div");
-    this.dopInfoWrapper.classList = "dopInfo";
-  }
-  handleForm() {
-    let visit = {
-      fio: this.form.fio.value,
-      purposeVisit: this.form.purposeVisit.value,
-      description: this.form.description.value,
-      origins: this.form.origins.value,
-      doctor: this.selectDoctor.value,
-    };
+	constructor() {
+		this.dopInfoWrapper = document.createElement('div');
+		this.dopInfoWrapper.classList = 'dopInfo';
+	}
+	handleForm() {
+		let visit = {
+			fio: this.form.fio.value,
+			purposeVisit: this.form.purposeVisit.value,
+			description: this.form.description.value,
+			origins: this.form.origins.value,
+			doctor: this.selectDoctor.value,
+		};
 
-    switch (this.selectDoctor.value) {
-      case "Кардіолог":
-        visit.pressure = this.pressure.value;
-        visit.indexMassa = this.indexMassa.value;
-        visit.ill = this.ill.value;
-        visit.age = this.age.value;
-        break;
-      case "Стоматолог":
-        visit.lastVisit = this.lastVisit.value;
-        break;
-      case "Терапевт":
-        visit.age = this.age.value;
-        break;
-      default:
-        break;
-    }
-      Requests.post(visit).then((data) => {
-      console.log(data);
+		switch (this.selectDoctor.value) {
+			case 'Кардіолог':
+				visit.pressure = this.pressure.value;
+				visit.indexMassa = this.indexMassa.value;
+				visit.ill = this.ill.value;
+				visit.age = this.age.value;
+				break;
+			case 'Стоматолог':
+				visit.lastVisit = this.lastVisit.value;
+				break;
+			case 'Терапевт':
+				visit.age = this.age.value;
+				break;
+			default:
+				break;
+		}
+		Requests.post(visit).then(data => {
+			console.log(data);
 
-      if (data.hasOwnProperty("id")) {
-        this.modal.remove();
-      }
-    });   
-  }
+			if (data.hasOwnProperty('id')) {
+				this.modal.remove();
+			}
 
-  
-  render() {
-    this.form = document.createElement("form");
-    this.form.classList.add("createVisit");
-    this.modal = document.createElement("div");
-    this.modal.classList = "div-createVisit";
-    let formTitle = document.createElement("h4");
-    this.selectDoctor = document.createElement("select");
-    let btnSubmit = document.createElement("button");
-    btnSubmit.classList.add("btnSubmit");
+			if (data.length === 0) {
+				const noItem = document.createElement('p');
+				noItem.innerText = 'No item has been added';
+				noItem.id = 'empty';
+				cardContainer.append(noItem);
+			} else {
+				let visitsObjects = Object.keys(data).map(visit => {
+					console.log(data[visit]);
 
-    btnSubmit.textContent = "Створити візит";
+					if (data[visit] === 'Стоматолог') {
+						const visitCard = new VisitDentist(data);
 
-    this.selectDoctor.innerHTML = ` 
+						visitCard.render(cardContainer);
+						return visitCard;
+					} else if (data[visit] === 'Кардіолог') {
+						const visitCard = new VisitCardiologist(visit);
+
+						visitCard.render(cardContainer);
+						return visitCard;
+					} else if (data[visit] === 'Терапевт') {
+						const visitCard = new VisitTherapist(visit);
+						visitCard.render(cardContainer);
+						return visitCard;
+					}
+				});
+				return visitsObjects;
+			}
+		});
+	}
+
+	render() {
+		this.form = document.createElement('form');
+		this.form.classList.add('createVisit');
+		this.modal = document.createElement('div');
+		this.modal.classList = 'div-createVisit';
+		let formTitle = document.createElement('h4');
+		this.selectDoctor = document.createElement('select');
+		let btnSubmit = document.createElement('button');
+		btnSubmit.classList.add('btnSubmit');
+
+		btnSubmit.textContent = 'Створити візит';
+
+		this.selectDoctor.innerHTML = ` 
         <option value="Лікар" selected="selected">Оберіть лікаря</option>
         <option value="Кардіолог">Кардіолог</option>
         <option value="Стоматолог">Стоматолог</option>
         <option value="Терапевт">Терапевт</option>`;
-    this.selectDoctor.classList = 'selectDoctor'
+		this.selectDoctor.classList = 'selectDoctor';
 
-    this.selectDoctor.addEventListener("change", () => {
-      this.dopInfoWrapper.innerHTML = "";
-      switch (this.selectDoctor.value) {
-        case "Кардіолог":
-          this.pressure = document.createElement("input");
-          this.pressure.setAttribute("name", "pressure");
-          this.pressure.placeholder = "Тиск";
+		this.selectDoctor.addEventListener('change', () => {
+			this.dopInfoWrapper.innerHTML = '';
+			switch (this.selectDoctor.value) {
+				case 'Кардіолог':
+					this.pressure = document.createElement('input');
+					this.pressure.setAttribute('name', 'pressure');
+					this.pressure.placeholder = 'Тиск';
 
-          this.indexMassa = document.createElement("input");
-          this.indexMassa.setAttribute("name", "indexMassa");
-          this.indexMassa.placeholder = "Індекс маси";
+					this.indexMassa = document.createElement('input');
+					this.indexMassa.setAttribute('name', 'indexMassa');
+					this.indexMassa.placeholder = 'Індекс маси';
 
-          this.ill = document.createElement("input");
-          this.ill.setAttribute("name", "ill");
-          this.ill.placeholder = "Перенесені захворювання";
+					this.ill = document.createElement('input');
+					this.ill.setAttribute('name', 'ill');
+					this.ill.placeholder = 'Перенесені захворювання';
 
-          this.age = document.createElement("input");
-          this.age.setAttribute("name", "age");
-          this.age.placeholder = "Вік";
+					this.age = document.createElement('input');
+					this.age.setAttribute('name', 'age');
+					this.age.placeholder = 'Вік';
 
-          this.dopInfoWrapper.append(
-            this.pressure,
-            this.indexMassa,
-            this.ill,
-            this.age
-          );
-          break;
-        case "Стоматолог":
-          this.lastVisit = document.createElement("input");
-          this.lastVisit.setAttribute("name", "lastVisit");
-          this.lastVisit.placeholder = "Останній візит" ;
-          this.dopInfoWrapper.append(this.lastVisit);
-          break;
+					this.dopInfoWrapper.append(this.pressure, this.indexMassa, this.ill, this.age);
+					break;
+				case 'Стоматолог':
+					this.lastVisit = document.createElement('input');
+					this.lastVisit.setAttribute('name', 'lastVisit');
+					this.lastVisit.placeholder = 'Останній візит';
+					this.dopInfoWrapper.append(this.lastVisit);
+					break;
 
-        case "Терапевт":
-          this.age = document.createElement("input");
-          this.age.setAttribute("name", "age");
-          this.age.placeholder = "Вік";
-          this.dopInfoWrapper.append(this.age);
-          break;
-      }
-    });
+				case 'Терапевт':
+					this.age = document.createElement('input');
+					this.age.setAttribute('name', 'age');
+					this.age.placeholder = 'Вік';
+					this.dopInfoWrapper.append(this.age);
+					break;
+			}
+		});
 
-    let fio = document.createElement("input");
-    fio.setAttribute("name", "fio");
-    fio.placeholder = "ПІБ";
+		let fio = document.createElement('input');
+		fio.setAttribute('name', 'fio');
+		fio.placeholder = 'ПІБ';
 
-    let origins = document.createElement("select");
-    origins.innerHTML = ` 
+		let origins = document.createElement('select');
+		origins.innerHTML = ` 
         <option value="Звичайна">Звичайна</option>
         <option value="Пріорітетна">Пріорітетна</option>
         <option value="Термінова">Термінова</option>`;
-    origins.setAttribute("name", "origins");
-    origins.classList = 'selectOrigins'
-    origins.placeholder = "Терміновість";
+		origins.setAttribute('name', 'origins');
+		origins.classList = 'selectOrigins';
+		origins.placeholder = 'Терміновість';
 
-    let description = document.createElement("input");
-    description.setAttribute("name", "description");
-    description.placeholder = "Короткий опис візиту";
+		let description = document.createElement('input');
+		description.setAttribute('name', 'description');
+		description.placeholder = 'Короткий опис візиту';
 
-    let purposeVisit = document.createElement("input");
-    purposeVisit.setAttribute("name", "purposeVisit");
-    purposeVisit.placeholder = "Мета візиту";
+		let purposeVisit = document.createElement('input');
+		purposeVisit.setAttribute('name', 'purposeVisit');
+		purposeVisit.placeholder = 'Мета візиту';
 
-    this.form.append(
-      fio,
-      purposeVisit,
-      description,
-      origins,
-      this.selectDoctor,
-      this.dopInfoWrapper,
-      btnSubmit
-    );
+		this.form.append(
+			fio,
+			purposeVisit,
+			description,
+			origins,
+			this.selectDoctor,
+			this.dopInfoWrapper,
+			btnSubmit,
+		);
 
-    this.form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      this.handleForm();
-    });
+		this.form.addEventListener('submit', e => {
+			e.preventDefault();
+			this.handleForm();
+		});
 
-    this.modal.append(this.form);
+		this.modal.append(this.form);
 
-    return this.modal;
-  }
+		return this.modal;
+	}
 }
-
-
